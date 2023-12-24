b98ed03699a413b0008a1eaf5aab8dff8ca5aca6
Fakhrirudin <76417345+Fakhrirudin@users.noreply.github.com>
2023-12-24 21:05:20 +0700

    Update list.jsx

--- components/list.jsx
+++ components/list.jsx
@@ -4,7 +4,7 @@ import { HiPencilAlt } from "react-icons/hi";
 
 const getJobs = async () => {
     try {
-        const res = await fetch("https://amazing-puffpuff-8c01b8.netlify.app", {
+        const res = await fetch("https://amazing-puffpuff-8c01b8.netlify.app/api/jobs", {
             cache: "no-store",
         });
 
